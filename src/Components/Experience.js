9476f5acae61871cd3ddfbd8395dc35220e0c131
Keith <Keith.macario@gmail.com>
2021-12-17 19:50:15 -0800

    Added Experience component. SchoolName, Major, Years added to the object. Need to figure out how to include it with app for the render.

---
+++ src/Components/Experience.js
@@ -0,0 +1,23 @@
+import render from "dom-serializer";
+import React, { Component } from "react";
+
+class Experience extends Component {
+    constructor(){
+      super();
+  
+      this.state = {
+        education: {
+          schoolName: '',
+          major: '',
+          years: '',
+        },
+        applicantInfo: [],
+      }
+    }
+}
+
+render({
+
+})
+
+export default Experience;
