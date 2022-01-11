d22e30c308c08594d51a5faa20ada67a6526e1a4
Keith <Keith.macario@gmail.com>
2022-01-11 01:27:24 -0800

    Added to experience info array map for to display user input.

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -4,7 +4,7 @@ import './Experience.css'
 
 
 class Experience extends Component {
-    constructor(){
+    constructor() {
         super()
 
         this.state = {
@@ -12,7 +12,7 @@ class Experience extends Component {
                 companyName: 'CVS',
                 position: 'Stocker',
                 mainTasks: 'description',
-                dateFRom: '2012 Mar',
+                dateFrom: '2012 Mar',
                 dateTo: '2020 Mar'
             }),
             infoArray: [],
@@ -20,51 +20,61 @@ class Experience extends Component {
         }
     }
 
-    render(){
-        const {experience, edit} = this.state;
-        if(edit){
-            return(
+    render() {
+        const { experience, edit, infoArray } = this.state;
+        if (edit) {
+            return (
                 <div>
-                    
+                    {infoArray.map((info) => {
+                        <div className={info}>
+                            <p>Company Name: {info.companyName}</p>
+                            <p>Position: {info.position}</p>
+                            <p>maintasks: {info.mainTasks}</p>
+                            <p>To: {info.dateTo}</p>
+                            <p>From: {info.dateFrom}</p>
+                        </div>
+                    })
+                    }
+
                 </div>
 
             )
             //show edit stuff
         }
         else {
-            return(
+            return (
                 <div>
-                <form className="edu" onSubmit={this.onSubmitInfo}>
-                    <div>
-                        <label className="companyName"> School </label>
-                        <input
-                        className="companyName"
-                        onChange={this.handleChange}
-                        placeholder = "Company Name"
-                        value = {experience.companyName}
-                        type = "text"
-                        name = "companyName"
-                        required
-                        />
-                        <label htmlFor="position"> position </label>
-                        <input
-                            className="position"
-                            onChange={this.handleChange}
-                            value={experience.position}
-                            type="text"
-                            name="major"
-                            required
-                        />
-                        <label htmlFor="dateTo"> Phone </label>
-                        <input
-                            className="dateTo"
-                            onChange={this.handleChange}
-                            value={education.dateOfSTudy}
-                            type="number"
-                            name="phone"
-                        />
-                    </div>
-                </form>
+                    <form className="edu" onSubmit={this.onSubmitInfo}>
+                        <div>
+                            <label className="companyName"> School </label>
+                            <input
+                                className="companyName"
+                                onChange={this.handleChange}
+                                placeholder="Company Name"
+                                value={experience.companyName}
+                                type="text"
+                                name="companyName"
+                                required
+                            />
+                            <label htmlFor="position"> position </label>
+                            <input
+                                className="position"
+                                onChange={this.handleChange}
+                                value={experience.position}
+                                type="text"
+                                name="major"
+                                required
+                            />
+                            <label htmlFor="dateTo"> Phone </label>
+                            <input
+                                className="dateTo"
+                                onChange={this.handleChange}
+                                value={education.dateOfSTudy}
+                                type="number"
+                                name="phone"
+                            />
+                        </div>
+                    </form>
                 </div>
         }
     }
