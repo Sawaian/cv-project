753b2dfd02fdbfc5026e48cb04e5b4d59a4bd2bb
Keith <Keith.macario@gmail.com>
2022-01-18 18:53:14 -0800

    Duplicate infoArray function.

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -19,61 +19,43 @@ class Experience extends Component {
             edit: false,
         }
     }
-        handleChange = (e) => {
+    handleChange = (e) => {
+        this.setState({
+            experience: ({ ...this.state.experience, [e.target.name]: e.target.value })
+        })
+    }
+
+    onSubmitInfo = (e) => {
+        e.preventDefault();
+        console.log(this.findDuplicate())
+        if (!this.findDuplicate()) {
+            console.log("Magic the gathering")
             this.setState({
-                experience: ({ ...this.state.experience, [e.target.name]: e.target.value })
-            })
+                infoArray: [...this.state.infoArray]
+            });
         }
-
-        onSubmitInfo = (e) => {
-            e.preventDefault();
-            if(this.state.infoArray.length !== 0){
-                this.checkForDuplicates();
-              }
-            this.handleEdit()
-            this.checkSubmission()
-        }
+        this.handleEdit()
+    }
 
 
-        checkSubmission = (e) => {
-            this.setState({
-                infoArray: [...this.state.infoArray, this.state.experience]
-            })
-        }
+ 
 
-        checkForDuplicates = (e) => {
-            if(this.state.infoArray.indexOf(this.state.experience !== -1)){
-                alert('Value not found in Array')
-                return false;
-            }
-        }
+    findDuplicate = () => {
+     return this.state.infoArray.find((x) => JSON.stringify(x) === JSON.stringify(this.state.experience)) 
+    }
 
-        // checkForDuplicates = (e) => {
-        //     //Iterate over array.
-        //     console.log("check initiated")
-        //     console.log(this.state.experience)
+    handleEdit = (e) => {
+        this.setState({
+            edit: true,
+        })
+    }
 
-        //     let info = this.state.infoArray
-            
-        //     for(let i = 0; i < info.length; i++){
-        //     if([i] === info.length){
-        //             console.log("push through")
-        //         }
-        //     }
-        // }
+    handleDisplay = (e) => {
+        this.setState({
+            edit: false,
+        })
+    }
 
-        handleEdit = (e) => {
-            this.setState({
-                edit: true,
-            })
-        }
-
-        handleDisplay = (e) => {
-            this.setState({
-                edit: false,
-            })
-        }
-    
 
     render() {
         const { experience, edit, infoArray } = this.state;
@@ -81,59 +63,59 @@ class Experience extends Component {
             return (
                 <div>
                     <button className="editBtn" onClick={this.handleDisplay}> Edit </button>
-                    { 
-                    infoArray.map((info) => (
-                        <div className={info}>
-                            <p>Company Name: {info.companyName}</p>
-                            <p>Position: {info.position}</p>
-                            <p>maintasks: {info.mainTasks}</p>
-                            <p>To: {info.dateTo}</p>
-                            <p>From: {info.dateFrom}</p>
-                        </div>
-                  )
+                    {
+                        infoArray.map((info) => (
+                            <div className={info}>
+                                <p>Company Name: {info.companyName}</p>
+                                <p>Position: {info.position}</p>
+                                <p>maintasks: {info.mainTasks}</p>
+                                <p>To: {info.dateTo}</p>
+                                <p>From: {info.dateFrom}</p>
+                            </div>
+                        )
+                        )
+                    }
+                </div>
             )
-            }
-            </div>
-            )
-            } else {
+        } else {
             return (
                 <div>
                     <form className="exp" onSubmit={this.onSubmitInfo}>
-                            <label className="companyName"> Company </label>
-                            <input
-                                className="companyName"
-                                onChange={this.handleChange}
-                                placeholder="Company Name"
-                                value={experience.companyName}
-                                type="text"
-                                name="companyName"
-                                required
-                            />
-                            <label htmlFor="position"> Position </label>
-                            <input
-                                className="position"
-                                onChange={this.handleChange}
-                                value={experience.position}
-                                type="text"
-                                name="major"
-                                required
-                            />
-                            <label htmlFor="dateTo"> Date To: </label>
-                            <input
-                                className="dateTo"
-                                onChange={this.handleChange}
-                                value={experience.dateTo}
-                                type="text"
-                                name="dateTo"
-                            />
-                            <label htmlFor="dateFrom"> Date From: </label>
-                            <input 
-                                className="dateFrom"
-                                onChange={this.handleChange}
-                                value={experience.dateFrom}
-                                type="text"
-                                name="dateFrom"
-                                />
+                        <label className="companyName"> Company </label>
+                        <input
+                            className="companyName"
+                            onChange={this.handleChange}
+                            placeholder="Company Name"
+                            value={experience.companyName}
+                            type="text"
+                            name="companyName"
+                            required
+                        />
+                        <label htmlFor="position"> Position </label>
+                        <input
+                            className="position"
+                            onChange={this.handleChange}
+                            value={experience.position}
+                            type="text"
+                            name="position"
+                            required
+                        />
+                        <label htmlFor="dateTo"> Date To: </label>
+                        <input
+                            className="dateTo"
+                            onChange={this.handleChange}
+                            value={experience.dateTo}
+                            type="text"
+                            name="dateTo"
+                        />
+                        <label htmlFor="dateFrom"> Date From: </label>
+                        <input
+                            className="dateFrom"
+                            onChange={this.handleChange}
+                            value={experience.dateFrom}
+                            type="text"
+                            name="dateFrom"
+                        />
                         <button className="submit" onSubmit={this.onSubmitInfo}> Add </button>
                     </form>
                 </div>
